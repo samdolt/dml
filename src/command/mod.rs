09b4d03e53bf28f392261a344f237e08a913d7d8
Samuel Dolt <samuel@dolt.ch>
2015-11-20 09:43:53 +0100

    Refactoring
    - Better documentation
    - Add run to impl Command

--- src/command/mod.rs
+++ src/command/mod.rs
@@ -1,20 +1,35 @@
+//! The `command` module contains helper function for performing interactive
+//! action.
+//!
+//! # Warning
+//!
+//! This function use stdin and stdout to communicate with a user.
+//!
+//! If you want to handle i/o and user interface, use functions of the
+//! `dml::helpers` module.
+
+mod process;
+use self::process::ProcessCommand;
+
+/// Available command on the command line
 #[derive(Debug, RustcDecodable)]
 pub enum Command {
     Build, New, Clean, Process,
 }
 
-trait DmlCommand {
-    fn run(args: &Vec<String>);
+impl Command {
+
+    /// Run the specified command with given argv
+    pub fn run(self, args: &Vec<String>) {
+        match self {
+            Command::Build      =>  println!("Build"),
+            Command::New        =>  println!("New"),
+            Command::Clean      =>  println!("Clean"),
+            Command::Process    =>  ProcessCommand::run(&args),
+        }
+    }
 }
 
-mod process;
-use self::process::ProcessCommand;
-
-pub fn run(cmd: Command, args: &Vec<String>) {
-    match cmd {
-        Command::Build      =>  println!("Build"),
-        Command::New        =>  println!("New"),
-        Command::Clean      =>  println!("Clean"),
-        Command::Process    =>  ProcessCommand::run(&args),
-    }
+trait DmlCommand {
+    fn run(argv: &Vec<String>);
 }
--- src/main.rs
+++ src/main.rs
@@ -7,19 +7,12 @@ extern crate log;
 extern crate env_logger;
 
 use docopt::Docopt;
-use std::io::stdout;
-use std::io::Write;
-use std::fs::File;
 use std::env;
 
-use dml::process;
-use dml::Format;
-use dml::Config;
 use dml::Command;
-use dml::command::run;
 
 const USAGE: &'static str = "
-DML - Dolt Markup Language
+dml - Dolt Markup Language
 
 Usage:
   dml <command> [<args>...]
@@ -29,13 +22,13 @@ Options:
   -h --help                 Show this screen.
   -v --version              Show version
 
-Some common DML commands are:
+Some common dml commands are:
   build     Compile the current project
   clean     Remove the target directory
   new       Create a new dml project
   process   Process a single file
 
-See 'cargo help <command>' for more information on a specific command.
+See 'dml help <command>' for more information on a specific command.
 ";
 
 #[derive(Debug, RustcDecodable)]
@@ -62,7 +55,7 @@ fn main() {
     env_logger::init().unwrap();
 
     match args.arg_command {
-        Some(cmd)   => run(cmd, &env::args().collect()),
+        Some(cmd)   => cmd.run(&env::args().collect()),
         None        => {
             if args.flag_version {
                 print_info();
@@ -71,35 +64,8 @@ fn main() {
             }
         }
     }
-
-
 }
 
 fn print_info() {
     println!("Dml version {}", env!("CARGO_PKG_VERSION"));
-}
-
-
-fn project_mode(args: &Args) {
-    unimplemented!()
-}
-
-fn single_file_mode(args: &Args) {
-
-
-    //let format = match args.flag_format.as_ref() {
-    //    "latex" => Format::Latex,
-    //    "html"  => Format::Html,
-    //    _       => panic!("Unknow ouput format: {}", args.flag_format)
-    //};
-
-    //let config = Config{
-    //    format: format,
-    //    with_header_and_footer: args.flag_ready,
-    //};
-
-    //match process(file, output, config) {
-    //    Ok(_)   => {},
-    //    Err(e)  => panic!("{}", e),
-    //}
 }
